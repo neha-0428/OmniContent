46c5b3bf3eff889dcfc42a2ed579e6c46c72a48d
Neha Kadam <neha@kitchenspurs.com>
2026-05-16 23:37:48 +0530

    created collection

--- src/controllers/collectionController.ts
+++ src/controllers/collectionController.ts
@@ -1,14 +1,28 @@
+import Collection from "@/models/Collection.js";
+import { AppError } from "@/utils/AppError.js";
 import { Request, Response } from "express";
 import expressAsyncHandler from "express-async-handler";
 
-
 export const createCollection = expressAsyncHandler(
-    async (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
+    const { name, fields } = req.body;
 
-        const { name, fields } = req.body
+    const collections = await Collection.create([
+      {
+        name: name,
+        fields: fields,
+      },
+    ]);
 
-        // const slug = name.slugify
+    const collection = collections[0];
 
-        
+    if (!collection) {
+      throw new AppError("Blueprint already exists!", 400);
     }
-)
+
+    res.status(201).json({
+      message: "Blueprint created successfully!",
+      data: collection,
+    });
+  },
+);
