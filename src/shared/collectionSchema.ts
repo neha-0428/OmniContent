b26bb25e1776d87d06f3f6ee84799ff5cd014f9f
Neha Kadam <neha@kitchenspurs.com>
2026-05-16 21:02:35 +0530

    created collection schema

---
+++ src/shared/collectionSchema.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+const fieldValidationSchema = z.object({
+    name: z.string()
+    .trim()
+    .min(1, "Field name cannot be empty")
+    .lowercase(),
+    type: z.enum(['text', 'number', 'boolean', 'rich-text'], {
+        error: () => ({ message: 'Type must be text, number, boolean, or rich-text' })
+    }),
+    required: z.boolean().default(false)
+})
+
+export const createCollectionSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(2, 'Name must be at least 2 characters long'),
+  fields: z
+    .array(fieldValidationSchema)
+    .min(1, "A collection must have at least one field"), // Prevents creating an empty blueprint
+});
