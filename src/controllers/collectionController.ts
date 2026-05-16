42b113e62a62507729544babbc76dbf8d6a68859
Neha Kadam <neha@kitchenspurs.com>
2026-05-16 23:20:06 +0530

    created collection controller

---
+++ src/controllers/collectionController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+import expressAsyncHandler from "express-async-handler";
+
+
+export const createCollection = expressAsyncHandler(
+    async (req: Request, res: Response) => {
+
+        const { name, fields } = req.body
+
+        
+    }
+)
