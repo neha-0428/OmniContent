0ae60e1d34fe24c9a8c772c24b9d1a724dbf8665
Neha Kadam <neha@kitchenspurs.com>
2026-05-17 19:32:55 +0530

    created an entry model

---
+++ src/models/Entry.ts
@@ -0,0 +1,61 @@
+import { model, Schema, Types } from "mongoose";
+
+interface EntryInterface {
+    orgId: Types.ObjectId,
+    collectionId: Types.ObjectId,
+    content: Record<string, any>,
+    status: 'Draft' | 'Published',
+    version: number
+    createdBy: Types.ObjectId,
+    updatedBy: Types.ObjectId,
+    createdAt: Date,
+    updatedAt: Date,
+}
+
+const entrySchema = new Schema<EntryInterface>(
+    {
+        orgId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'Organisation',
+            index: true
+        },
+        collectionId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'Collection',
+        },
+        content: {
+            type: Schema.Types.Mixed,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['Draft', 'Published'],
+            default: 'Draft',
+            index: true
+        },
+        version: {
+            type: Number,
+            default: 1
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        updatedBy: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        }
+    }, { timestamps: true }
+)
+
+entrySchema.index({ orgId: 1, collectionId: 1})
+
+entrySchema.index({ orgId: 1, collectionId: 1, status: 1 });
+
+const Entry = model<EntryInterface>('Entry', entrySchema);
+
+export default Entry
